0752ce6605fc7ec9c97eeb84c5f37eeb4f6e1a00
guienjoris <49984305+guienjoris@users.noreply.github.com>
2019-07-05 14:47:09 +0200

    fini

--- index.js
+++ index.js
@@ -8,25 +8,39 @@ const monInput = document.getElementById('inputToDo');
 const mesToDo = document.getElementById('maToDoList');
 const monContenu = document.getElementById('contenu');
 let monStorage = localStorage.getItem('monNom');
+let quote = document.getElementById('quote');
+let citation= ["Il y a 10 types de personnes dans le monde : celles qui comprennent le binaire, et celles qui ne le comprennent pas.",
+"Microsoft : « Vous avez des questions ? Nous avons des trombones qui dansent. »",
+"Si vous ne réussissez pas du premier coup, appelez ça « version 1.0 ».",
+"La programmation aujourd’hui est une course entre les développeurs tâchant de concevoir des programmes <br> de plus en plus nombreux et efficaces, convenant même aux imbéciles, et l’univers essayant de produire <br>des idiots de plus en plus nombreux et efficaces.<br> Jusqu’à présent, c’est l’univers qui gagne.",
+"J’adorerais changer le monde, mais ils ne veulent pas me fournir le code source."];
 
-
+let maDate = new Date;
+let monHeure = maDate.getHours() + "<span> : </span>" + maDate.getMinutes() +"<span> : </span>" + maDate.getSeconds();
+if (maDate.getMinutes() <10) {
+    monHeure = maDate.getHours() + "<span> : 0 </span>" + maDate.getMinutes() +"<span> : </span>" + maDate.getSeconds();
+}
+if (maDate.getSeconds() <10) {
+    monHeure = maDate.getHours() + "<span> : </span>" + maDate.getMinutes() +"<span> : 0 </span>" + maDate.getSeconds();
+}
+monContenu.style.backgroundImage = `url(./images/${maDate.getHours()}.jpg)`;
 
 //Fonction temps réel
 setInterval(() => {
     let maDate = new Date;
+
     let monHeure = maDate.getHours() + "<span> : </span>" + maDate.getMinutes() +"<span> : </span>" + maDate.getSeconds();
     if (maDate.getMinutes() <10) {
-        monHeure = maDate.getHours() + "<span> : </span>" +"<span> 0 </span>"+ maDate.getMinutes() +"<span> : </span>" + maDate.getSeconds();
+        monHeure = maDate.getHours() + "<span> : 0 </span>" + maDate.getMinutes() +"<span> : </span>" + maDate.getSeconds();
     }
     if (maDate.getSeconds() <10) {
-        monHeure = maDate.getHours() + "<span> : </span>" + maDate.getMinutes() +"<span> : </span>" +"<span> 0 </span>"+ maDate.getSeconds();
+        monHeure = maDate.getHours() + "<span> : </span>" + maDate.getMinutes() +"<span> : 0 </span>" + maDate.getSeconds();
     }
 
     // Affichage de l'heure
     monTitre.innerHTML = monHeure;
 
     //Image en fonction de l'heure
-    monContenu.style.backgroundImage = `url(./images/${maDate.getHours()}.jpg)`;
     console.log(maDate.getHours());
     console.log(monContenu);
 
@@ -35,16 +49,35 @@ setInterval(() => {
 
 // récupération du nom
 window.onload = () => { 
-    monNom.value = monStorage;
     monContenu.style.backgroundImage = `url(./images/${maDate.getHours()}.jpg)`;
     monTitre.innerHTML = monHeure;
+    let number= Math.random();
+    if (number < 0.20){
+        quote.innerHTML = citation[0];
+    }
+    else if (number < 0.40 && number > 0.20){
+        quote.innerHTML = citation[1];
+    }
+    else if (number < 0.60 && number > 0.40){
+        quote.innerHTML = citation[2];
+    }
+    else if (number < 0.80 && number > 0.60){
+        quote.innerHTML = citation[3];
+    }
+    else{
+        quote.innerHTML = citation[4];
+    }
+    console.log(citation[0]);
+
+    if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(maPosition);
+    console.log(maPosition);
 } 
 
 // local storage
 monNom.addEventListener('change', () => {
     localStorage.setItem('monNom', monNom.value);
     monStorage = localStorage.getItem('monNom');
-    monNom.value = monStorage;
     console.log(monStorage);
 })
 
@@ -54,22 +87,26 @@ fetch(`https://api.apixu.com/v1/current.json?key=25aff8fbdd17436eb17103421192305
     .then(data => {
         console.log(data);
         maTemperature.textContent = data.current.temp_c + "°C";
-        monLieu.textContent = data.location.name;
+        monLieu.textContent = "Localisation: " + data.location.name;
         monIcone.setAttribute('src', data.current.condition.icon);
     })
 
+// 
+    
+const todoStock =[];
+
 //TaskList
-monInput.addEventListener('change', () => {
+    monInput.addEventListener('change', () => {
     let newTask = document.createElement('li');
     let innerTask = document.createElement('label');
     let btnTask = document.createElement('input');
     let deleteBtn = document.createElement('button');
-
+    
+        
     mesToDo.appendChild(newTask); //Nouvelle Li
     newTask.appendChild(btnTask); //Ajout du label
     newTask.appendChild(innerTask); //Ajout de la checkbox
     newTask.appendChild(deleteBtn); //Ajout du X
-
     deleteBtn.innerHTML = "X";
 
     //attribut de la checkbox
@@ -80,6 +117,10 @@ monInput.addEventListener('change', () => {
     innerTask.setAttribute('for', 'maValeur');
     innerTask.innerHTML = monInput.value;
 
+    todoStock.push( monInput.value);
+    localStorage.setItem('todo', JSON.stringify(todoStock));
+    monInput.value="";   
+    
     //strike checkbox
     btnTask.addEventListener('click', () => {
         if (!btnTask.checked) {
@@ -87,11 +128,14 @@ monInput.addEventListener('change', () => {
         }else{
             innerTask.style.textDecoration = 'line-through';
         }
-    })
 
+    })
     //suppression task
     deleteBtn.addEventListener('click', () => {
         mesToDo.removeChild(newTask);
     })
+
     
 })
+
+
